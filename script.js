be4701aae7a107ec258ad43d0de4660cd9f53b18
Hunmza54 <saeedh20@mcmaster.ca>
2022-11-18 23:58:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let weather = {
+let weather = { //have to figure out the API data fetching
     apikey: "e6bb28fee9184ef89e743934221911",
     fetch(
         "https://api.weatherapi.com/v1/current.json?"+apikey+"="+city+"Toronto&aqi=yes";
